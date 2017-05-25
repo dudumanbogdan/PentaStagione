0119285fde704555f540f25ae01d570625643e2c
stefa08 <stefan.pascaru08@gmail.com>
2017-05-25 13:20:50 +0300

    Added User management application page.

---
+++ PentaStagione.WebApp/AngularJs/Services/requests.js
@@ -0,0 +1,20 @@
+﻿(() => {
+    angular.module('app.pizza').service('requests',
+    [
+        '$http', function ($http) {
+            this.getUsers = function (cb,err) {
+                $http.get('https://jsonplaceholder.typicode.com/posts/1').then(cb).catch(err);
+            };
+            this.UpdateUser = function (user, succ) {
+                var url = "http://localhost:4900/api/users";
+                $http.post(url,user).then(succ).catch(() => { console.log("error on edit user"); });
+            };
+            this.DeleteUser = function (id, succ) {
+                var url = "http://localhost:4900/api/users/"+id;
+                $http.delete(url).then(succ).catch(() => { console.log("error on delete user"); });
+            };
+        }
+    ]);
+})()
+
+//http\://localhost:49900/api/pizza
---
+++ PentaStagione.WebApp/AngularJs/app.pizza.js
@@ -0,0 +1,26 @@
+﻿(() => {
+    angular.module('app.pizza', ['ngRoute'])
+        .config([
+            '$routeProvider', ($routeProvider) => {
+                $routeProvider
+                    .when('/users',
+                    {
+                        templateUrl: 'AngularJs/Views/Users.html',
+                        controller: 'usersController'
+                    })
+                    .when('/newUser',
+                    {
+                        templateUrl: 'AngularJs/Viwes/newUser.html',
+                        controller: 'usersController'
+                    })
+                     .when("/users/edit/:id",
+                    {
+                        templateUrl: "AngularJs/Views/EditUser.html",
+                        controller: "usersController"
+                    })
+                .otherwise({
+                    redirectTo: '/'
+                });
+            }
+        ]);
+})();
